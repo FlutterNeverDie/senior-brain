cd3c66a821bad02a2b51d5539ec285845c6e0270
Sanghoon Lee <nasi3611@naver.com>
2026-04-20 13:48:55 +0900

    feat : 앱 로고

--- granite.config.ts
+++ granite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   brand: {
     displayName: "부모님 치매예방",
     primaryColor: "#FF6B9D",
-    icon: "",
+    icon: "https://static.toss.im/appsintoss/16823/d0e0d433-614e-4f36-97d3-a7127ce120fb.png",
   },
   web: {
     host: "localhost",
